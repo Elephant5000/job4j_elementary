30594035679a4d20fd4b21e61f385209911fb5a8
Elephant5000 <pashau@mail.ru>
2020-05-23 21:32:15 +0300

    1.1.4.5.8. JavaDoc. Code convention [#294008]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,16 +1,36 @@
 package ru.job4j.converter;
 
+/*
+* Converter
+*
+* @author Pavel Ustyuzhanin
+ */
 public class Converter {
+
+    /*
+    * rubTousd
+    * @param value - value.
+    * @return conversion from rubles to dollars
+    */
     public static int rubTousd(int value) {
         int rsl = value / 75;
         return rsl;
     }
 
+    /*
+     * rubToeur
+     * @param value - value.
+     * @return conversion from rubles to euro
+     */
     public static int rubToeur(int value) {
         int rsl = value / 85;
         return rsl;
     }
 
+    /*
+     * main
+     * @param args - args.
+     */
     public static void main(String[] args) {
         int in = 170;
         int expected = 2;
