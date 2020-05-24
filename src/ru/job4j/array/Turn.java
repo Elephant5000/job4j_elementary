08432aa24ab2ff74652a24431a88107d8eb943dc
Elephant5000 <pashau@mail.ru>
2020-05-24 17:37:39 +0300

    1.1.5.6.2.3. Перевернуть массив. [#293938]

---
+++ src/ru/job4j/array/Turn.java
@@ -0,0 +1,12 @@
+package ru.job4j.array;
+
+public class Turn {
+    public static int[] back(int[] array) {
+        SwitchArray switchArray = new SwitchArray();
+        for (int index = 0; index < (int) (array.length / 2); index++ ) {
+            switchArray.swap(array, index, (array.length - index - 1));
+        }
+        return array;
+    }
+
+}
