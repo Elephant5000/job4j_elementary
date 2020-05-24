c13cd03431794e11811a9e1c96783c26bd913f59
Elephant5000 <pashau@mail.ru>
2020-05-24 22:13:01 +0300

    1.1.5.6.8. Дефрагментация массива. [#293942]

---
+++ src/ru/job4j/array/Defragment.java
@@ -0,0 +1,29 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int indexNull = 0; indexNull < array.length; indexNull++) {
+            if (array[indexNull] == null) {
+                for (int indexNotNull = indexNull + 1; indexNotNull < array.length; indexNotNull++) {
+                    if (array[indexNotNull] != null) {
+                        String temp = array[indexNull];
+                        array[indexNull] = array[indexNotNull];
+                        array[indexNotNull] = temp;
+                        break;
+                    }
+                }
+            }
+            System.out.print(array[indexNull] + " ");
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        System.out.println();
+        for (int index = 0; index < compressed.length; index++) {
+            System.out.print(compressed[index] + " ");
+        }
+    }
+}
