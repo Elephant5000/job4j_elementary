9a6c5043689a3bd07b20bd1f291f552f133dda2e
Elephant5000 <pashau@mail.ru>
2020-05-25 08:44:12 +0300

    1.1.5.6.6.1. Двухмерный массив. Вывод на консоль. [#293941] внесены корректировки в код

--- src/ru/job4j/array/BarleyBreak.java
+++ src/ru/job4j/array/BarleyBreak.java
@@ -5,8 +5,8 @@ public class BarleyBreak {
         int[][] array1 = { {1,2,3}, {4,5,6}, {7,8,9} };
         int[][] array2 = new int[3][3];
         int index = 0;
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+        for (int i = 0; i < array2.length; i++) {
+            for (int j = 0; j < array2.length; j++) {
                 index++;
                 array2[i][j] = index;
             }
