4e867f4227d7f48cdc98eafb171a51dbf0182d72
Elephant5000 <pashau@mail.ru>
2020-05-28 20:05:03 +0300

    1.2.1.2.2. Рефакторинг - Площадь треугольника. [#294086]

--- test/ru/job4j/condition/TriangleTest.java
+++ test/ru/job4j/condition/TriangleTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.condition;
 
+import org.junit.Assert;
 import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -7,8 +8,24 @@ import static org.junit.Assert.assertThat;
 public class TriangleTest {
 
     @Test
-    public void whenExist() {
-        boolean result = Triangle.exist(2.0, 2.0, 2.0);
-        assertThat(result, is(true));
+    public void areaRealTriangle() {
+        Point a = new Point(0, 0);
+        Point b = new Point(1, 0);
+        Point c = new Point(1, 1);
+        Triangle triangle = new Triangle(a, b, c);
+        double expected = 0.5;
+        double out = triangle.area();
+        Assert.assertEquals(expected, out, 0.1);
+    }
+
+    @Test
+    public void areaUnRealTriangle() {
+        Point a = new Point(0, 0);
+        Point b = new Point(1, 0);
+        Point c = new Point(2, 0);
+        Triangle triangle = new Triangle(a, b, c);
+        double expected = -1;
+        double out = triangle.area();
+        Assert.assertEquals(expected, out, 0.1);
     }
 }
